63f572e84d74f4737338d742bc062ef809b43cbd
JorgeCamToledo <camacho.ja0402@gmail.com>
2023-04-10 18:05:13 -0600

    datefield added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vaca_cloud/pages/Login.dart';
 import 'package:vaca_cloud/pages/Prueba.dart';
-
 import 'package:vaca_cloud/pages/CreateAnimal.dart';
 
 void main() async {
